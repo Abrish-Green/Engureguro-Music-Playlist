b0dee53f8dc21a576080b7087c354bd9ab848f0e
Abrish-Green <abrham365muche@gmail.com>
2022-08-02 19:40:06 +0300

    Edited Header.tsx

--- src/Layout/Header/Index.tsx
+++ src/Layout/Header/Index.tsx
@@ -12,19 +12,19 @@ export const Header = () => {
             <div className="site-name text-3xl font-logo-font-style font-bold  text-white hidden md:block">Engureguro</div>
           </div>              
         </div>
-        <div className="right-header border-white col-span-4 text-white p-5">
+        <div className="right-header border-white col-span-4 text-white p-7">
           <ul className="gap-2 float-right hidden md:inline-flex">
             <li>
-              <a className="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium" href="#">Live</a>
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-md font-medium" href="#">Live</a>
             </li>
             <li>
-              <a className="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium" href="#">Categories</a>
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-md font-medium" href="#">Categories</a>
             </li>
             <li>
-              <a className="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium" href="#">Upload</a>
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-md font-medium" href="#">Upload</a>
             </li>
             <li>
-              <a className="text-gray-100 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium" href="#">Login</a>
+              <a className="text-gray-300 text-gray-100 hover:ring-2 hover:shadow-md px-5 py-2 rounded-md text-md font-medium ring-1" href="#">Login</a>
             </li>
             
             
