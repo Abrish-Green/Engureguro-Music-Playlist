4a484902e3428490ae966c5ec5377231d0ca66dd
Abrish-Green <abrham365muche@gmail.com>
2022-08-02 14:25:26 +0300

    Added Header Layout

--- src/Layout/Header/Index.tsx
+++ src/Layout/Header/Index.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
           </div>              
         </div>
         <div className="right-header border-white col-span-4 text-white p-5">
-          <ul className="inline-flex gap-2 float-right">
+          <ul className="gap-2 float-right hidden md:inline-flex">
             <li>
               <a className="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium" href="#">Live</a>
             </li>
