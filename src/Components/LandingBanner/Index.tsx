9f8f445a7d128c31d8aa179172979760b0b0cb2d
Abrish-Green <abrham365muche@gmail.com>
2022-08-05 12:09:46 +0300

    Added Landing Banner

--- src/Components/LandingBanner/Index.tsx
+++ src/Components/LandingBanner/Index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
  const LandingBanner = () => {
   return (
       <>
-        <div className="w-full h-[55vh] bg-black">
+        <div className="w-full h-auto bg-black">
               <div className="into w-full border-white border text-white grid grid-cols-3 py-4">
                   <div></div>
                   <div>
@@ -18,9 +18,12 @@ import React from 'react'
                               <input type="text" className="border-none w-96 h-10 py-2 px-4" placeholder="Search for Genre, Tag or Category"/>
                             </div>
                       <div className="artist-tags">
-                              <span className="border border-white py-1 px-5 mx-1 rounded-full">span 1</span>
-                              <span className="border border-white py-1 px-5 mx-1 rounded-full">span 1</span>
-                              <span className="border border-white py-1 px-5 mx-1 rounded-full">span 1</span>
+                              <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
+                              <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
+                              <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
+                              <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
+                              <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
+                              <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
                               
                       </div>
                       </div>
