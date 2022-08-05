42c8924753da8b3431d32a21f4b27c6893b84d3f
Abrish-Green <abrham365muche@gmail.com>
2022-08-05 22:01:12 +0300

    Break Down Landing Page into Components

--- src/Components/LandingBanner/SearchTags.tsx
+++ src/Components/LandingBanner/SearchTags.tsx
@@ -1,5 +1,46 @@
 import { motion } from 'framer-motion';
-export const SearchTags = () => {
+const data: Array<Object> = [
+    {
+        title: "button",
+        link: ""
+    },
+    {
+        title: "button",
+        link: ""
+    },
+    {
+        title: "button",
+        link: ""
+    },
+    {
+        title: "button",
+        link: ""
+    },{
+        title: "button",
+        link: ""
+    },
+    {
+        title: "button",
+        link: ""
+    },
+    {
+        title: "button",
+        link: ""
+    },
+    {
+        title: "button",
+        link: ""
+    }
+]
+
+interface SearchDataList {
+    data?: SearchData[]
+}
+interface SearchData {
+    title: string,
+    link: string 
+}
+export const SearchTags = ( {data}: SearchDataList) => {
     return (
         <>
             <motion.div
@@ -7,12 +48,12 @@ export const SearchTags = () => {
                 animate={{ opacity:1, y: 0 }}
                 transition={{ duration: 0.8 }}
                 className="artist-tags">
-                                <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
-                                <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
-                                <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
-                                <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
-                                <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
-                                <button className="border border-white py-1 px-5 m-1 rounded-full">button 1</button>
+                                <button className="border border-white py-.5 px-5 m-1 rounded-full">button 1</button>
+                                <button className="border border-white py-.5 px-5 m-1 rounded-full">button 1</button>
+                                <button className="border border-white py-.5 px-5 m-1 rounded-full">button 1</button>
+                                <button className="border border-white py-.5 px-5 m-1 rounded-full">button 1</button>
+                                <button className="border border-white py-.5 px-5 m-1 rounded-full">button 1</button>
+                                <button className="border border-white py-.5 px-5 m-1 rounded-full">button 1</button>
                                 
             </motion.div>
         </>
