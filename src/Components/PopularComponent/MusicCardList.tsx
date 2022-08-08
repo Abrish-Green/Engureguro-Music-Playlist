7ea6bf0f2820b61fb98ff04433f96b96a0c880eb
Abrish-Green <abrham365muche@gmail.com>
2022-08-08 14:40:21 +0300

    Added Popular Music Component

--- src/Components/PopularComponent/MusicCardList.tsx
+++ src/Components/PopularComponent/MusicCardList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MusicCard } from './MusicCard';
+import { motion } from 'framer-motion';
 
 interface MusicCardListProps{
     data?: {
@@ -78,8 +79,16 @@ const MusicCardList = ({ data = [] }: MusicCardListProps) => {
         <div className="px-24 py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:px-32 md:py-12 gap-5">
             {
                   sampleData.data && sampleData.data.map((music_card) => <MusicCard />)
-            }  
-        </div>
+              }  
+          </div>
+          <motion.div
+              initial={{y:-50}}
+                animate={{ y: 0 }}
+                key={'see-more'}
+                transition= {{ duration: 0.8 }}
+              className="px-24 py-10 grid grid-cols-3">
+              <button className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-2  rounded-lg  flex items-center justify-center w-32 md:w-44 dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 cols-span-2">See More</button> 
+          </motion.div>
       </>
   )
 }
