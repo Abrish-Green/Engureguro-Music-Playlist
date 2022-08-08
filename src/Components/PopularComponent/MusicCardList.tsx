70fcb752fa379bd184a6f224f60a3408ad74a322
Abrish-Green <abrham365muche@gmail.com>
2022-08-08 14:43:51 +0300

    Added Popular Music Component

--- src/Components/PopularComponent/MusicCardList.tsx
+++ src/Components/PopularComponent/MusicCardList.tsx
@@ -86,8 +86,9 @@ const MusicCardList = ({ data = [] }: MusicCardListProps) => {
                 animate={{ y: 0 }}
                 key={'see-more'}
                 transition= {{ duration: 0.8 }}
-              className="px-24 py-10 grid grid-cols-3">
-              <button className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-2  rounded-lg  flex items-center justify-center w-32 md:w-44 dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 cols-span-2">See More</button> 
+              className="px-24 py-10 flex justify-center">
+             
+              <button className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-2  rounded-lg  flex items-center justify-center w-32 md:w-44 dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400">See More</button> 
           </motion.div>
       </>
   )
