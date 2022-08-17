27442227be99f9fe2e9258777422d4330262e333
Abrish-Green <abrham365muche@gmail.com>
2022-08-17 14:30:42 +0300

    Recreated Music Card Component

--- src/Components/ListFeaturedMusic/ListOfFeaturedMusics.tsx
+++ src/Components/ListFeaturedMusic/ListOfFeaturedMusics.tsx
@@ -4,12 +4,15 @@ import SingleMusicCard from '../SingleMusicCard/Index'
  const ListOfFeaturedMusics = () => {
   return (
       <>
-          <div className="w-full h-auto gap-2 p-2 mt-2 flex justify-center">
-              <SingleMusicCard />
-          </div>
-          <div className="w-full h-auto gap-2 p-2 mt-2 flex justify-center">
-              <SingleMusicCard />
-          </div>
+          {
+              [...Array(10)].map((item) => {
+                  return (
+                    <div className="w-full h-auto gap-2 p-2 mt-2 flex justify-center">
+                            <SingleMusicCard />
+                    </div>
+                        )
+            })
+          }
            
       
       </>
--- src/Components/SingleMusicCard/Index.tsx
+++ src/Components/SingleMusicCard/Index.tsx
@@ -1,18 +1,37 @@
 import React from 'react'
-
+import { BsPlayCircle } from 'react-icons/bs'
+import { GrLike } from 'react-icons/gr'
+import { BiCommentDetail, BiShare } from 'react-icons/bi'
+import { FaDownload } from 'react-icons/fa'
  const SingleMusicCard = () => {
   return (
     <>
-      <div className="music-card flex-col border w-[70%] h-72 p-2">
-        <div className="photo border h-1/2">
+      <div className="music-card flex-col md:grid md:grid-rows-1 md:grid-cols-8 border w-[70%] h-80 md:h-44 p-2">
+        <div className="photo h-1/2 md:h-full col-span-2">
             <img src="https://zephyrcreates.com/wp-content/uploads/2019/06/music-covers-WEB-zephyr-02.jpg" alt="" className="w-full h-full object-cover" />
         </div>
-        <div className="info border h-1/2 flex-col">
-          <div className="info h-auto border inline-flex">
-            <div className="">Icon</div>
-            <div className="">Title</div>
+        <div className="info h-1/2 flex-col overflow-clip p-2 md:h-full col-span-6">
+          <div className="info h-auto border-b inline-flex gap-2 p-2 w-full items-center">
+            <div className="text-3xl"><BsPlayCircle /></div>
+            <div className="break-all text-lg font-bold">Like I'm Gonna Lose You
+              <div className="text-sm font-normal font-md">By Artist</div>
+            </div>
           </div>
-          <div className="interact h-auto border">Like and share</div>
+          <div className="interact h-auto inline-flex gap-2 px-2 items-center text-lg py-4 p-2 w-full">
+            <button className="like inline-flex gap-1 items-center"><GrLike />
+              <span className='text-sm font-semibold'>0</span>
+            </button>
+            <button className="comment inline-flex gap-1 items-center">
+              <BiCommentDetail />
+            </button>
+            <button className="share inline-flex gap-1 items-center">
+              <BiShare />Share
+            </button>
+            <button className="download inline-flex gap-1 items-center ">
+              <FaDownload />
+              <span className='hidden md:flex'>Download</span>
+            </button>
+          </div>
         </div>
       </div>
     </>
